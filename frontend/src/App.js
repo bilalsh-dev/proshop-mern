9eda4de13135ced6619fae3623ac4a1a9651331f
bilalshahid929 <bilalshahid929@gmail.com>
2022-10-23 11:12:13 -0700

    react-router-dom and react-router-bootstrap added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="App py-3">
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
