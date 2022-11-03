cfa9ae7c5b8ee1970020b9a22e7a112c34e9e792
bilalshahid929 <bilalshahid929@gmail.com>
2022-11-02 23:56:23 -0700

    Products Routes added , Express asnyc handler added

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -11,17 +12,18 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+app.use("/api/products", productRoutes);
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
-app.get("/messages", (req, res) => {
-  res.send("Hello");
-});
+// app.get("/api/products", (req, res) => {
+//   res.json(products);
+// });
+// app.get("/api/products/:id", (req, res) => {
+//   const product = products.find((p) => p._id === req.params.id);
+//   res.json(product);
+// });
+// app.get("/messages", (req, res) => {
+//   res.send("Hello");
+// });
 
 app.get("/:universalURL", (req, res) => {
   res.send("404 URL NOT FOUND");
