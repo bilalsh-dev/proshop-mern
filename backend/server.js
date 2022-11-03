ae18948b2f13d056be87978ddb9aae87f4cec4c1
bilalshahid929 <bilalshahid929@gmail.com>
2022-11-03 00:53:15 -0700

    error handling

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productRoutes.js";
@@ -13,6 +14,8 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 // app.get("/api/products", (req, res) => {
 //   res.json(products);
@@ -25,9 +28,9 @@ app.use("/api/products", productRoutes);
 //   res.send("Hello");
 // });
 
-app.get("/:universalURL", (req, res) => {
-  res.send("404 URL NOT FOUND");
-});
+// app.get("/:universalURL", (req, res) => {
+//   res.send("404 URL NOT FOUND");
+// });
 
 const PORT = process.env.PORT || 5000;
 
